99c1d604dbd2f1905bc3756c38ba305cb8b6371c
zhaoJing96 <1850218650@qq.com>
2019-07-09 10:44:14 +0800

    迁移项目

--- app.js
+++ app.js
@@ -1,32 +1,67 @@
-const api = require('utils/API.js');
 //app.js
 App({
   onLaunch: function () {
-    // 获取地理位置
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userLocation']) {
-          wx.getLocation({
-            success: function (res) {
-              var params = {
-                latitude: res.latitude,
-                longitude: res.longitude,
-                speed: res.speed,
-                accuracy: res.accuracy,
-                altitude: res.altitude,
-                verticalAccuracy: res.verticalAccuracy,
-                horizontalAccuracy: res.horizontalAccuracy
-              }
-              api.updateLocation(params, { showLoading: false })
-            },
-          })
-        }
-      }
-    });
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
 
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url: 'http://yunbaite.csoft.com/appApi/?p=wx&m=login',
+          data: Object.assign({ js_code: res.code }),
+          method: "post",
+          header: {
+            'content-type': 'application/json'
+          },
+          success:  (res)=> {
+            console.log(res.data)
+            this.globalData.token = res.data.token
+            // 获取用户信息
+            wx.getSetting({
+              success: res => {
+                if (res.authSetting['scope.userInfo']) {
+                  // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                  wx.getUserInfo({
+                    success: res => {
+                      // console.log("app_getUserInfo", res);
+                      // 可以将 res 发送给后台解码出 unionId
+                      wx.request({
+                        url: 'http://yunbaite.csoft.com/appApi/?p=wx&m=getUserInfo',
+                        data: Object.assign({ encryptedData: res.encryptedData, iv: res.iv, token: this.globalData.token }),
+                        method: "post",
+                        header: {
+                          'content-type': 'application/json'
+                        },
+                        success:  (res)=> {
+                          this.globalData.token=res.data.token
+                          console.log('getUserInfo result token:',res.data.token)
+                        }
+                      })
+
+                      this.globalData.userInfo = res.userInfo
+
+                      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                      // 所以此处加入 callback 以防止这种情况
+                      if (this.userInfoReadyCallback) {
+                        this.userInfoReadyCallback(res)
+                      }
+                    }, lang: "zh_CN"
+                  })
+                }
+              }
+            })
+          }
+        })
+      }
+    })
+    
   },
-  onHide: function () {
-    // 清除所有缓存
-    wx.clearStorage();
+  globalData: {
+    userInfo: null,
+    token:null
   }
-});
+})
--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // API 接口
-  API_HOST: "http://jdwx.yunbaite.cn",//开发版
-  HTTPS_API_HOST:"https://xcx.yunbaite.com"//体验版
-}
